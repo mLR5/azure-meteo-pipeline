17215cfff45f6957dc9f1912ba4e83a70a336ec0
mLR5 <lecomte.marc.5@gmail.com>
2025-04-10 21:50:36 -0400

    ajout de la function app avec declencheur http + bronze layer

---
+++ bronze/function-app/datacollect/__init__.py
@@ -0,0 +1,34 @@
+import logging
+import requests
+import json
+import os
+import azure.functions as func  
+from azure.functions import HttpRequest, HttpResponse
+
+def get_weather(city):
+    url = "http://api.weatherapi.com/v1/current.json"
+    params = {
+        "key": os.environ.get("WEATHER_API_KEY", ""),  # clé API via env var
+        #"key": "",
+        "q": city
+    }
+    response = requests.get(url, params=params)
+    return response.json()
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Azure Function météo déclenchée')
+
+    try:
+        body = req.get_json()
+        villes = body.get("villes", ["Paris", "Lille", "Amiens"])
+    except:
+        villes = ["Paris", "Lille", "Amiens"]
+
+    resultats = []
+
+    for ville in villes:
+        data = get_weather(ville)
+        resultats.append({"ville": ville, "data": data})
+
+    return func.HttpResponse(json.dumps(resultats), mimetype="application/json")
