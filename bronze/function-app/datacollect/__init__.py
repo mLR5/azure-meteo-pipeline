3a56cc6677728215d4dd75bd7f00e4f5d8b30cd2
mLR5 <lecomte.marc.5@gmail.com>
2025-04-12 21:06:10 -0400

    test key

--- bronze/function-app/datacollect/__init__.py
+++ bronze/function-app/datacollect/__init__.py
@@ -8,8 +8,8 @@ from azure.functions import HttpRequest, HttpResponse
 def get_weather(city):
     url = "http://api.weatherapi.com/v1/current.json"
     params = {
-        "key": os.environ.get("WEATHER_API_KEY", ""),  # clé API via env var
-        #"key": "",
+        #"key": os.environ.get("WEATHER_API_KEY", ""),  # clé API via env var
+        "key": "4dec28902a7040929ca70430232812",
         "q": city
     }
     response = requests.get(url, params=params)
